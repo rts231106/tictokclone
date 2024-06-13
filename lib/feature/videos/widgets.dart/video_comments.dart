57b12d76638c2e18299dcf030e777c07d9493620
DGyoon24 <gyoon00125@gmail.com>
2024-06-13 17:01:24 +0900

    comments

--- lib/feature/videos/widgets.dart/video_comments.dart
+++ lib/feature/videos/widgets.dart/video_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tictokclone/constants/gaps.dart';
 import 'package:tictokclone/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -36,11 +37,74 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index) => Container(
-                  child: const Text('im comment'),
-                )),
+        body: ListView.separated(
+          separatorBuilder: (context, index) => Gaps.v20,
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size10,
+            horizontal: Sizes.size16,
+          ),
+          itemCount: 10,
+          itemBuilder: (context, index) => Container(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CircleAvatar(
+                  radius: 18,
+                  child: Text("d"),
+                ),
+                Gaps.h10,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "data",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: Sizes.size14,
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                      Gaps.v3,
+                      const Text(
+                          "asdalskdjaslkdjaslkdjaslkjdaskldjaslkdjaslkjdlaksjdaslkjdalksj")
+                    ],
+                  ),
+                ),
+                Gaps.h10,
+                Column(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.heart,
+                      size: Sizes.size20,
+                      color: Colors.grey.shade500,
+                    ),
+                    Gaps.v2,
+                    Text(
+                      "52.2k",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 18,
+                backgroundColor: Colors.grey.shade500,
+                foregroundColor: Colors.white,
+                child: const Text("d"),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

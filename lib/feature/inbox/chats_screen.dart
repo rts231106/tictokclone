145282704a5b4861f2e5f63ef71b750f8ec19488
DGyoon24 <gyoon00125@gmail.com>
2024-06-16 22:24:35 +0900

    AnimatedListparttwo

--- lib/feature/inbox/chats_screen.dart
+++ lib/feature/inbox/chats_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tictokclone/constants/sizes.dart';
+import 'package:tictokclone/feature/inbox/chat_detail_screen.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -15,17 +16,76 @@ class _ChatScreenState extends State<ChatScreen> {
   final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
 
   final List<int> _items = [];
+  final Duration _duration = const Duration(milliseconds: 200);
 
   void _addItem() {
     if (_key.currentState != null) {
       _key.currentState!.insertItem(
         _items.length,
-        duration: const Duration(
-          milliseconds: 300,
-        ),
+        duration: _duration,
       );
+
       _items.add(_items.length);
     }
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+          index,
+          (context, animation) => SizeTransition(
+                sizeFactor: animation,
+                child: _makeTile(index),
+              ),
+          duration: _duration);
+    }
+    _items.remove(index);
+  }
+
+  void _onChatTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ChatDetailScreen(),
+      ),
+    );
+  }
+
+  Widget _makeTile(int index) {
+    return ListTile(
+      onLongPress: () => _deleteItem(index),
+      onTap: _onChatTap,
+      leading: const CircleAvatar(
+        radius: 30,
+        foregroundImage:
+            NetworkImage("https://avatars.githubusercontent.com/u/3612017"),
+        child: Text(
+          "dong ",
+        ),
+      ),
+      //trailing 을 title과 위쪽으로 위치를 정렬하고 싶음
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            "gyoon $index",
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            "2:16 pm",
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size12,
+            ),
+          ),
+        ],
+      ),
+      subtitle: const Text(
+        "Don't for get to make video",
+      ),
+    );
   }
 
   @override
@@ -55,42 +115,8 @@ class _ChatScreenState extends State<ChatScreen> {
           return FadeTransition(
             key: UniqueKey(),
             opacity: animation,
-            child: SizeTransition(
-              sizeFactor: animation,
-              child: ListTile(
-                leading: const CircleAvatar(
-                  radius: 30,
-                  foregroundImage: NetworkImage(
-                      "https://avatars.githubusercontent.com/u/3612017"),
-                  child: Text(
-                    "dong ",
-                  ),
-                ),
-                //trailing 을 title과 위쪽으로 위치를 정렬하고 싶음
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      "gyoon $index",
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      "2:16 pm",
-                      style: TextStyle(
-                        color: Colors.grey.shade500,
-                        fontSize: Sizes.size12,
-                      ),
-                    ),
-                  ],
-                ),
-                subtitle: const Text(
-                  "Don't for get to make video",
-                ),
-              ),
-            ),
+            child:
+                SizeTransition(sizeFactor: animation, child: _makeTile(index)),
           );
         },
       ),
